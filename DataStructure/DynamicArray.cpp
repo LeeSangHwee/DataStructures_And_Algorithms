8faafe5940609a43e017397873f170b79e9d7581
LeeSangHwee <tkdgnl56@naver.com>
2024-03-12 19:46:59 +0900

    first push

---
+++ DataStructure/DynamicArray.cpp
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+// Dynamic Array(동적 배열)
+void funcArray(int n)
+{
+	int* numArray = (int*)malloc(sizeof(int) * n);
+	free(numArray);
+}
+
+int main()
+{
+	funcArray(6);
+}
+
---
+++ DataStructure/StaticArray.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+// Static Array(정적 배열)
+int main()
+{
+	int numArray[4];
+
+	numArray[0] = 0;
+	numArray[1] = 1;
+	numArray[2] = 2;
+	numArray[3] = 3;
+}
+
