a53fe1ff254f5c23c7b2672bcaa3ee0697c8c993
LeeSangHwee <tkdgnl56@naver.com>
2024-07-03 17:49:36 +0900

    Circular Linked List

--- DataStructure/CircularLinkedList.cpp
+++ DataStructure/CircularLinkedList.cpp
@@ -9,44 +9,51 @@ enum eINPUT {
 	eEXIT
 };
 
+enum eINSERT {
+	eFIRST_INSERT = 1,
+	eMIDDLE_INSERT,
+	eLAST_INSERT
+};
+
 typedef struct Node
 {
 	int Data;
-	struct Node* Previous;
 	struct Node* Next;
 }Node;
 
 int SelectUI();
-Node* Insert_Node(Node* Head);
-Node* Delete_Node(Node* Head);
-void Print_Node(Node* Head);
-void Exit_Program(Node* Head);
+Node* InsertNode(Node* List_ptr, Node** Head);
+void Delete_Node(Node** List_ptr, Node** Head);
+void Print_Node(Node* List_ptr, Node* Head);
+void Exit_Program(Node* List_ptr, Node* Head);
 
 int main()
 {
 	bool ProgramExit = false;
 	Node* Head = NULL;
+	Node* List_ptr = NULL;
 
 	while (!ProgramExit)
 	{
 		switch (SelectUI())
 		{
-		case eINSERT://지금은 Head에 추가하는 방식임 원하는 위치에 추가하는 방식을 넣고싶으면 삭제를 응용하면됨
-			Head = Insert_Node(Head);
+		case eINSERT:
+			List_ptr = InsertNode(List_ptr, &Head);
+			if (Head == NULL) Head = List_ptr;
 			break;
 		case eDELETE:
-			if (Head == NULL) printf("삭제할 노드가 존재하지 않습니다!\n");
-			else Head = Delete_Node(Head);
+			if (List_ptr == NULL) printf("삭제할 노드가 존재하지 않습니다!\n");
+			else Delete_Node(&List_ptr, &Head);
 			break;
 		case ePRINT:
-			if (Head == NULL) printf("출력할 노드가 존재하지 않습니다!\n");
-			else printf("노드 리스트 : "), Print_Node(Head), printf("\n");
+			if (List_ptr == NULL) printf("출력할 노드가 존재하지 않습니다!\n");
+			else printf("노드 리스트 : "), Print_Node(List_ptr, Head), printf("\n");
 			break;
 		case eEXIT:
-			Exit_Program(Head);
+			Exit_Program(List_ptr, Head);
 			ProgramExit = true;
 			break;
-		default:break;
+		default: break;
 		}
 		system("pause");
 		system("cls");
@@ -66,88 +73,139 @@ int SelectUI()
 	printf("4. Exit\n");
 	printf("====================\n");
 	printf("입력 : "), scanf_s("%d", &input);
+	system("cls");
 
 	return input;
 }
 
-Node* Insert_Node(Node* Head)
+Node* InsertNode(Node* List_ptr, Node** Head)
 {
-	Node* NewNode = (Node*)malloc(sizeof(Node));
+	int input = 0;
+
+	while (1 > input || input > 3)
+	{
+		printf("====Select Insert Method====\n");
+		printf("1. First_Insert\n");
+		printf("2. Middle_Insert\n");
+		printf("3. Last_Insert\n");
+
+		printf("====================\n");
+		printf("입력 : "), scanf_s("%d", &input);
+		system("cls");
+	}
+
+	// 신규 노드 생성
+	Node* NewNode = (Node*)malloc(sizeof(Node));	
 	printf("삽입할 노드 입력 : "), scanf_s("%d", &(NewNode->Data));
-	NewNode->Previous = NULL;
-	NewNode->Next = Head;
+	// 노드가 하나도 존재하지 않을 경우
+	if (List_ptr == NULL)
+	{
+		NewNode->Next = List_ptr;
+		List_ptr = NewNode;
+	}		
+	// 노드가 하나라도 존재할 경우
+	else
+	{
+		switch (input)
+		{
+		// 리스트 첫번째 위치에 삽입
+		case eFIRST_INSERT:
+			NewNode->Next = List_ptr;
+			(*Head)->Next = NewNode;
+			List_ptr = NewNode;
+			break;
+		// 리스트 중간에 삽입
+		case eMIDDLE_INSERT:
+			{
+			MIDDLE_INSERT_INPUT:
+				int input = 0;
+				printf("삽입할 노드의 이전 데이터 값 입력 : "), scanf_s("%d", &input);
 
-	if (Head != NULL)
-		Head->Previous = NewNode;
+				Node* TempNode = List_ptr;
+				while (true)
+				{
+					if(TempNode->Data == input)
+					{
+						NewNode->Next = TempNode->Next;
+						TempNode->Next = NewNode;
+						break;
+					}
+					else if (TempNode == *Head)
+					{
+						printf("현재 찾고 있는 이전 데이터의 값이 존재하지 않습니다! 다시 입력해주세요.");
+						system("pause");
+						system("cls");
 
-	return NewNode;
+						goto MIDDLE_INSERT_INPUT;
+					}
+					TempNode = TempNode->Next;
+				}
+			}
+			break;
+		// 리스트 마지막에 삽입
+		case eLAST_INSERT:
+			{
+				NewNode->Next = List_ptr;
+				(*Head)->Next = NewNode;
+				*Head = NewNode;
+			}
+			break;
+		default:break;
+		}
+	}
+	return List_ptr;
 }
 
-Node* Delete_Node(Node* Head)
+void Delete_Node(Node** List_ptr, Node** Head)
 {
 DELETE_INPUT:
 	int input;
 	printf("삭제할 노드의 데이터 입력 : "), scanf_s("%d", &input);
 
-	Node* ReturnNode = NULL;
-	Node* SearchNode = Head;
+	Node* TempNode = *Head;
+	Node* SearchNode = *List_ptr;
 
 	while (true)
 	{
-		if (SearchNode == NULL)
+		if (SearchNode->Data == input)
 		{
-			printf("삭제하려는 노드가 존재하지 않습니다! 다시 입력해주세요.\n");
+			// 삭제 노드가 List_ptr 인 경우
+			if (SearchNode == *List_ptr)
+				*List_ptr = SearchNode->Next;
+			// 삭제 노드가 Head 인 경우
+			else if (SearchNode == *Head)
+				*Head = TempNode;
+
+			TempNode->Next = SearchNode->Next;
+			free(SearchNode);
+			break;
+		}
+		else if (SearchNode == *Head)
+		{
+			printf("삭제할 노드가 존재하지 않습니다! 다시 입력해주세요.\n");
 			system("pause");
 			system("cls");
 
 			goto DELETE_INPUT;
 		}
-		else if (SearchNode->Data == input)
-		{
-			if (SearchNode->Previous == NULL)
-			{
-				if (SearchNode->Next != NULL) // 탐색된 원소가 맨 왼쪽일 경우
-				{
-					SearchNode->Next->Previous = NULL;
-					ReturnNode = SearchNode->Next;
-				}
-				else if (SearchNode->Next == NULL) // 탐색된 원소가 하나만 존재할 경우
-					ReturnNode = NULL;
-			}
-			else if (SearchNode->Previous != NULL)
-			{
-				if (SearchNode->Next != NULL)  // 탐색된 원소가 리스트 중간에 있는 경우
-				{
-					SearchNode->Previous->Next = SearchNode->Next;
-					SearchNode->Next->Previous = SearchNode->Previous;
-				}
-				else if (SearchNode->Next == NULL)  // 탐색된 원소가 맨 오른쪽일 경우
-					SearchNode->Previous->Next = NULL;
 
-				ReturnNode = Head;
-			}
-			free(SearchNode);
-			return ReturnNode;
-		}
+		TempNode = SearchNode;
 		SearchNode = SearchNode->Next;
 	}
-	return NULL;
 }
 
-void Print_Node(Node* Head)
+void Print_Node(Node* List_ptr, Node* Head)
 {
-	if (Head != NULL)
-	{
-		Print_Node(Head->Next);
-		printf("%d ", Head->Data);
-	}
+	if (List_ptr != Head)
+		Print_Node(List_ptr->Next, Head);	
+
+	printf("%d ", List_ptr->Data);
 }
 
-void Exit_Program(Node* Head)
+void Exit_Program(Node* List_ptr, Node* Head)
 {
-	if (Head != NULL)
-	{
-		Exit_Program(Head->Next);
-		free(Head);
-	}
+	if (List_ptr != Head)
+		Exit_Program(List_ptr->Next, Head);
+
+	free(List_ptr);
 }
--- DataStructure/TEST.cpp
+++ DataStructure/TEST.cpp
@@ -2,16 +2,39 @@
 #include "stdlib.h"
 #include "Windows.h"
 
-int* testfunc(int* num1)
-{
-	int* num2;
-
-	return num2;
-}
+typedef struct str
+{	
+	str* ptr1;
+	str* ptr2;
+	int data;
+};
 
 int main()
 {
+	//str* a = (str*)malloc(sizeof(str));
+	//str b = *a;
+	//str& c  = *a;
+	//str& d  = b;
+	//
+	//printf("%d %d %d %d\n", a, b, c, d);
+	//printf("%d %d %d %d\n", &a, &b, &c, &d);
+	//printf("%d\n", &a->ptr1);
 
+	int a = 0;
+	int* b = &a;
+	int& c = *b;
+	int& d = (int&)b;
+	int** e = &b;
+
+	int i = 5;
+	int ii = i;
+	int iii = (int&)i;	
+	int* iiii= &i;
+	int& iiiii = (int&)iiii;
+	
+	printf("%d", sizeof(int&));
+
+	//printf("%d %d %d %d\n", i, &j, &i, j);
 
 	return 0;
 }
